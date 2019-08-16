aa605f7d41c47eb99b9036cb16607f592f850b72
Brendan A R Sechter <sgeos@hotmail.com>
2019-08-16 16:18:24 +0900

    Broken work in progress.

--- Chapter02/tetris/src/main.rs
+++ Chapter02/tetris/src/main.rs
@@ -1,12 +1,15 @@
 extern crate palette;
 extern crate sdl2;
+extern crate sdl2_sys;
 
 use palette::{Hsv, LinSrgb};
 use sdl2::event::Event;
+use sdl2::image::{LoadSurface, InitFlag};
 use sdl2::keyboard::Keycode;
-use sdl2::pixels::Color;
+use sdl2::pixels::{Color, PixelFormatEnum};
 use sdl2::rect::Rect;
 use sdl2::render::{Texture, TextureCreator};
+use sdl2::surface::Surface;
 use sdl2::video::WindowContext;
 use std::error::Error;
 use std::thread::sleep;
@@ -36,10 +39,15 @@ impl ColorBase {
 pub fn main() -> Result<(), Box<Error>> {
     let sdl_context = sdl2::init()?;
     let video_subsystem = sdl_context.video()?;
+    sdl2::image::init(InitFlag::PNG | InitFlag::JPG)?;
     let window = video_subsystem.window("Tetris", 800, 600).position_centered().opengl().build()?;
     let mut canvas = window.into_canvas().target_texture().present_vsync().build()?;
     let texture_creator: TextureCreator<_> = canvas.texture_creator();
     let mut square_texture: Texture = create_texture_square(&texture_creator, TEXTURE_SIZE)?;
+    let mut background_surface: Surface = Surface::from_file("assets/background.png")?;
+    unsafe {
+      sdl2_sys::SDL_ConvertSurfaceFormat((&mut background_surface).as_ptr() as *mut sdl2_sys::SDL_Surface, sdl2_sys::SDL_PIXELFORMAT_RGBA8888 as u32, 0);
+    }
 
     let mut done = false;
     let mut frame: u64 = 0;
@@ -72,6 +80,7 @@ pub fn main() -> Result<(), Box<Error>> {
                 texture.clear();
             }
         )?;
+        //canvas.copy(&background_texture, None, None)?;
         canvas.copy(&square_texture, None, Rect::new(0, 0, TEXTURE_SIZE, TEXTURE_SIZE))?;
         canvas.present();
         sleep(Duration::new(0, SLEEP_SECOND / FPS));
@@ -80,15 +89,35 @@ pub fn main() -> Result<(), Box<Error>> {
     Ok(())
 }
 
+fn create_hue_shifted_texture_from_surface<'a>(
+    texture_creator: &'a TextureCreator<WindowContext>,
+    surface: &'a Surface
+) -> Result<Texture<'a>, sdl2::render::TextureValueError> {
+    let width = surface.width();
+    let height = surface.height();
+    let pitch = surface.pitch();
+    let src_pixels: *const u8 = (*surface.raw()).pixels as *const u8;
+    let texture = texture_creator.create_texture_target(PixelFormatEnum::RGBA8888, width, height)?;
+    let dst_pixels: *mut u8 = (*texture.raw()).pixels as *mut u8;
+    if src_pixels.is_null() || dst_pixels.is_null() {
+      return sdl2::render::TextureValueError::SdlError("No pixel data.");
+    } else if pitch < 3 {
+      return sdl2::render::TextureValueError::SdlError("Unexpected pitch.");
+    }
+    //for i in 0..width*height*pitch {
+    //}
+    texture
+}
+
 fn create_texture_square<'a>(
     texture_creator: &'a TextureCreator<WindowContext>,
     size: u32
 ) -> Result<Texture<'a>, sdl2::render::TextureValueError> {
-    texture_creator.create_texture_target(None, size, size)
+    texture_creator.create_texture_target(PixelFormatEnum::RGBA8888, size, size)
 }
 
 fn hsv_to_color(hsv: Hsv) -> Color {
     let rgb = LinSrgb::from(hsv);
-    Color::RGB((255.0 * rgb.red) as u8, (255.0 * rgb.green) as u8, (255.0 * rgb.blue) as u8)
+    Color::RGBA((255.0 * rgb.red) as u8, (255.0 * rgb.green) as u8, (255.0 * rgb.blue) as u8, 255)
 }
 
--- README.md
+++ README.md
@@ -2,6 +2,19 @@
 
 A repository to use while working through the book Rust Programming By Example.
 
+## Notes ##
+
+It might be necessary to tell Rust where the SDL2 libraries have been installed.
+For example, add something like this to *~/.profile*.
+
+```sh
+# Homebrew Installation
+export LIBRARY_PATH="$LIBRARY_PATH:/usr/local/lib"
+
+# MacPorts Installation
+export LIBRARY_PATH="$LIBRARY_PATH:/opt/local/lib"
+```
+
 ## References
 
 - [Rust Programming By Example, Official Repository][book-official-repo]
